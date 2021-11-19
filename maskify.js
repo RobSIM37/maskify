deb628d94ff0906cf1a9d306210dc38b8ba8078b
Robert Lewis <robsim37@gmail.com>
2021-11-19 08:16:09 -0500

    initial commit

---
+++ maskify.js
@@ -0,0 +1,9 @@
+const maskify=(str,show)=>str.length>show?"#".repeat(str.length-show) + str.slice(-show):str.slice(-show);
+
+let currentShow = 4;
+console.log(maskify('Hello World!', currentShow));
+console.log(maskify('3133886596', currentShow));
+console.log(maskify('Robert C. Lewis', currentShow));
+console.log(maskify('112233445566778', currentShow));
+console.log(maskify('12', currentShow));
+console.log(maskify('', currentShow));
